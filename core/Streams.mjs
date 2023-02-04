311e20d33b0f289d55638ca983a0fdf82e5443d5
Mugamba669 <brunohectre@gmail.com>
2023-02-05 01:35:34 +0300

    Added search global search of songs

--- core/Streams.mjs
+++ core/Streams.mjs
@@ -1,7 +1,7 @@
 import  get  from "axios";
 import { load } from "cheerio";
 export let hot100songs = [];
-
+let songContainer = [];
 get(`https://www.nowviba.com/music/pages/top100.php`).then((dom)=>{
             var response = dom.data;
             const ch = load(response);
@@ -25,10 +25,37 @@ export const searchHot100Track = (track) => {
     return hot100songs.find((song) => song.title == track);
 }
 
-/** *
- * @param {string} artist|songName
+/**
+ * @param {string} query
+ * @Mugamba669
+ * @returns {ArrayBufferConstructor}
  * 
  */
-export const searchAllSongs = (artist) => {
-    
+export const searchAllSongs = (query) => {
+    let searchSource = `https://www.nowviba.com/music/pages/search.php?q=${query}`;
+    if(typeof query != 'string') console.error("Only strings are allowed");
+
+    get(searchSource).then((response)=>{
+            var html = response.data;
+            const ch = load(html);
+            var trackList = ch('.container a');
+       trackList.map((index,track) => {
+           var element = track;
+        var more = (`${element.children[0].children[1].attribs.style}`).replace("background-image:","");
+            let sourceFile;
+            get(`https://www.nowviba.com/music/pages/${element.attribs.href}`).then((response)=>{
+                var detail = response.data;
+                var ch = load(detail);
+               sourceFile =  ch('.dwnTkNt').attr().href;
+        });
+        var stream = {
+            title: element.children[0].children[3].children[1].children[0].data,
+            artist:element.children[0].children[3].children[3].children[0].data,
+           coverArt:more.replace('url(',"").replace(");",""),
+            url:sourceFile,
+          };
+          songContainer = [...songContainer, stream];
+       });
+    });
+    return searchAllSongs;
 }
--- server.mjs
+++ server.mjs
@@ -1,11 +1,32 @@
 import express from 'express';
-import { hot100songs } from './core/Streams.mjs';
+import { hot100songs, searchHot100Track,searchAllSongs } from './core/Streams.mjs';
 
-const app = express();
-app.get(`/api/songs`,(req, res) => {
- 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(hot100songs));
-    res.end();
-console.log(`Server running at http://localhost:${process.env.PORT || 5054} `)
-}).listen(process.env.PORT || 5054);
+try {
+      const app = express();
+    console.log(`Server running at http://localhost:${process.env.PORT || 5054} `)
+
+    // Fetching only hot 100 Ug songs
+    app.get(`/api/hotSongs`,(req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.json(hot100songs);
+        res.end();
+    });
+
+    // search from hot100 songs
+    app.get('/api/searchHot/:query',(req,res)=>{
+    
+      res.json(searchHot100Track(req.params.query));
+      res.end();
+    })
+
+    // search songs from all Ugandan songs by title or artist
+    app.get('/api/searchAll/:queryList',(req,res)=>{
+      req.params;
+      res.json(searchAllSongs(req.params.queryList));
+      res.end();
+    });
+    app.listen(process.env.PORT || 5054);
+
+} catch (error) {
+  console.error(error);
+}
