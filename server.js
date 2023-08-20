24b5d5154f8fab4c5a9192f1e23291edf2369d85
Mugamba Bruno <brunohectre@gmail.com>
2023-08-20 04:00:09 +0300

    server removed unwanted

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
 const express = require('express');
-const { fetchHot100 } = require('./core/Streams.js');
-
+// const { fetchHot100 } = require('./core/Streams.js');
+const app = express();
 try {
-  const app = express();
+
 
   // Fetching only hot 100 Ug songs
-  app.get('/api/hotSongs', async (req, res) => {
-    console.log(`Requesting at http://localhost:${process.env.PORT || 5054}/api/hotSongs `);
+  app.get('/', (req, res) => {
+    // console.log(`Requesting at http://localhost:${process.env.PORT || 5054}/api/hotSongs `);
     try {
-      res.json(await fetchHot100())
+      res.send("Hello World")
 
     } catch (e) {
       res.send(e).status(400);
     }
-    console.log("Done fetching")
+
     res.end();
 
   });
-  // fetch new songs
-  app.get("/api/newSongs", async (req, res) => {
-    console.log(`Requesting at http://localhost:${process.env.PORT || 5054}/api/hotSongs `);
-    await fetchNewSongs();
-    res.json({ "response": "200" });
-    console.log("Done fetching")
-    res.end();
-  })
+  // // fetch new songs
+  // app.get("/api/newSongs", async (req, res) => {
+  //   console.log(`Requesting at http://localhost:${process.env.PORT || 5054}/api/hotSongs `);
+  //   await fetchNewSongs();
+  //   res.json({ "response": "200" });
+  //   console.log("Done fetching")
+  //   res.end();
+  // })
   // search from hot100 songs
-  app.get('/api/searchHot/:query', (req, res) => {
-    res.json(searchHot100Track(req.params.query));
-    res.end();
-  })
+  // app.get('/api/searchHot/:query', (req, res) => {
+  //   res.json(searchHot100Track(req.params.query));
+  //   res.end();
+  // })
 
-  // search songs from all Ugandan songs by title or artist
-  app.get('/api/searchAll/:queryList', (req, res) => {
-    res.json(searchAllSongs(req.params.queryList));
-    res.end();
-  });
+  // // // search songs from all Ugandan songs by title or artist
+  // app.get('/api/searchAll/:queryList', (req, res) => {
+  //   res.json(searchAllSongs(req.params.queryList));
+  //   res.end();
+  // });
   app.listen(process.env.PORT || 5054);
   console.log(`Server running at http://localhost:${process.env.PORT || 5054} `)
 
 } catch (error) {
   console.error(error);
 }
+
+module.exports = app;
