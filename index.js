edd1e51337220f4de640dd2d8ec8c1464991c925
Mugamba Bruno <brunohectre@gmail.com>
2023-08-20 11:14:33 +0300

    fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
+const express = require('express');
 const app = express()
 app.all('/', (req, res) => {
 
   res.send("Am working")
   res.end();
 })
-app.listen(process.env.PORT || 5054)
+app.listen(process.env.PORT || 5054);
