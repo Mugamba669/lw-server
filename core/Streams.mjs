b6f2c738385aea5f931ce17688d8f7f18696f8a7
Mugamba Bruno <brunohectre@gmail.com>
2023-08-20 03:38:55 +0300

    modified server

--- core/Streams.mjs
+++ core/Streams.mjs
@@ -1,28 +1,53 @@
-import  get  from "axios";
+import get from "axios";
 import { load } from "cheerio";
 export let hot100songs = [];
 let songContainer = [];
-get(`https://www.nowviba.com/music/pages/top100.php`).then((dom)=>{
-            var response = dom.data;
-            const ch = load(response);
-            let trackList = ch('.hot100')
-        trackList.map((index,element)=>{
-            let data = {
-                title:`${element.children[0].children[5].children[0].children[0].data}`,
-                artist:`${element.children[0].children[5].children[1].children[0].data}`,
-                artWork:`${element.children[0].children[3].children[1].attribs.data}`,
-                url:element.children[2].children[3].children[1].attribs.src
-            }
-             
-            hot100songs = [...hot100songs, data];
+export const fetchHot100 = async () => {
+    let songs = [];
+    await get(`https://www.nowviba.com/music/pages/top100.php`).then((domResponse) => {
+        let response = domResponse.data;
+        const ch = load(response);
+        let trackList = ch('.hot100');
+        trackList.map((index, element) => {
+            let d = {
+                title: `${element.children[0].children[5].children[0].children[0].data}`,
+                artist: `${element.children[0].children[5].children[1].children[0].data}`,
+                artWork: `${element.children[0].children[3].children[1].attribs.data}`,
+                url: `${element.children[2].children[3].children[1].attribs.src}`
+            };
+            songs = [...songs, d];
         });
-    });
+    })
+    hot100songs = songs;
+    return songs;
+}
+
+export const fetchNewSongs = async () => {
+    let songs = [];
+    await get(`https://www.nowviba.com/music/pages/newhot.php`).then((domResponse) => {
+        let response = domResponse.data;
+        const ch = load(response);
+        let trackList = ch('.songcd');
+        trackList.map((index, element) => {
+            // let d = {
+            //     title: `${element.children[0].children[5].children[0].children[0].data}`,
+            //     artist: `${element.children[0].children[5].children[1].children[0].data}`,
+            //     artWork: `${element.children[0].children[3].children[1].attribs.data}`,
+            //     url: `${element.children[2].children[3].children[1].attribs.src}`
+            // };
+            console.log(element.attribs.src);
+            // songs = [...songs, d];
+        });
+    })
+    // hot100songs = songs;
+    // return songs;
+}
 
 /**
  * @param {string} track name
  */
 export const searchHot100Track = (track) => {
-    return hot100songs.find((song) => song.title == track);
+    return hot100songs.filter((song) => song.title == track);
 }
 
 /**
@@ -33,29 +58,29 @@ export const searchHot100Track = (track) => {
  */
 export const searchAllSongs = (query) => {
     let searchSource = `https://www.nowviba.com/music/pages/search.php?q=${query}`;
-    if(typeof query != 'string') console.error("Only strings are allowed");
+    if (typeof query != 'string') console.error("Only strings are allowed");
 
-    get(searchSource).then((response)=>{
-            var html = response.data;
-            const ch = load(html);
-            var trackList = ch('.container a');
-       trackList.map((index,track) => {
-           var element = track;
-        // var more = (`${element.children[0].children[1].attribs.style}`).replace("background-image:","");
+    get(searchSource).then((response) => {
+        var html = response.data;
+        const ch = load(html);
+        var trackList = ch('.container a');
+        trackList.map((index, track) => {
+            var element = track;
+            // var more = (`${element.children[0].children[1].attribs.style}`).replace("background-image:","");
             let sourceFile;
-            get(`https://www.nowviba.com/music/pages/${element.attribs.href}`).then((response)=>{
+            get(`https://www.nowviba.com/music/pages/${element.attribs.href}`).then((response) => {
                 var detail = response.data;
                 var ch = load(detail);
-               sourceFile =  ch('.dwnTkNt').attr().href;
+                sourceFile = ch('.dwnTkNt').attr().href;
+            });
+            var stream = {
+                // title: element.children[0].children[3].children[1].children[0].data,
+                // artist:element.children[0].children[2].children[3].children[0].data,
+                //    coverArt:more.replace('url(',"").replace(");",""),
+                url: sourceFile,
+            };
+            songContainer = [...songContainer, stream];
         });
-        var stream = {
-            // title: element.children[0].children[3].children[1].children[0].data,
-            // artist:element.children[0].children[2].children[3].children[0].data,
-        //    coverArt:more.replace('url(',"").replace(");",""),
-            url:sourceFile,
-          };
-          songContainer = [...songContainer, stream];
-       });
     });
     return searchAllSongs;
 }
---
+++ server.mjs
@@ -0,0 +1,44 @@
+import express from 'express';
+import { fetchNewSongs, searchHot100Track, searchAllSongs, fetchHot100 } from './core/Streams.mjs';
+
+try {
+  const app = express();
+
+  // Fetching only hot 100 Ug songs
+  app.get('/api/hotSongs', async (req, res) => {
+    console.log(`Requesting at http://localhost:${process.env.PORT || 5054}/api/hotSongs `);
+    try {
+      res.json(await fetchHot100())
+
+    } catch (e) {
+      res.send(e).status(400);
+    }
+    console.log("Done fetching")
+    res.end();
+
+  });
+  // fetch new songs
+  app.get("/api/newSongs", async (req, res) => {
+    console.log(`Requesting at http://localhost:${process.env.PORT || 5054}/api/hotSongs `);
+    await fetchNewSongs();
+    res.json({ "response": "200" });
+    console.log("Done fetching")
+    res.end();
+  })
+  // search from hot100 songs
+  app.get('/api/searchHot/:query', (req, res) => {
+    res.json(searchHot100Track(req.params.query));
+    res.end();
+  })
+
+  // search songs from all Ugandan songs by title or artist
+  app.get('/api/searchAll/:queryList', (req, res) => {
+    res.json(searchAllSongs(req.params.queryList));
+    res.end();
+  });
+  app.listen(process.env.PORT || 5054);
+  console.log(`Server running at http://localhost:${process.env.PORT || 5054} `)
+
+} catch (error) {
+  console.error(error);
+}
